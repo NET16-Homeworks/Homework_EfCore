b326b6013264413df9b5b3311b1a667e3711ba7b
Dissident26 <blackhate@bk.ru>
2022-11-30 16:28:01 +0300

    Web Api preparations: removed all views removed redundant files

--- Homework_EfCore/Homework_EfCore/Program.cs
+++ Homework_EfCore/Homework_EfCore/Program.cs
@@ -1,14 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using Homework_EfCore;
 
-builder.Services.AddControllersWithViews();
+CreateHostBuilder(args)
+    .Build()
+    .Run();
 
-var app = builder.Build();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapControllerRoute("default",
-                       "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+static IHostBuilder CreateHostBuilder(string[] args)
+{
+    return Host.CreateDefaultBuilder(args)
+               .ConfigureWebHostDefaults(webBuilder =>
+               {
+                   webBuilder.UseStartup<Startup>();
+               });
+}
---
+++ Homework_EfCore/Homework_EfCore/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace Homework_EfCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen();
+
+            // services.AddInMemoryUserManagement();
+
+            // services.AddUserManagementByFile();
+
+            // services.AddEfCoreUserManagement(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{Assembly.GetExecutingAssembly().GetName().Name} v1");
+            });
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            //app.UseMiddleware<ErrorHandlerMiddleware>();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
